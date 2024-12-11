83e1a90a6018a5bab2d82991c7ffb406b0119d8a
Rinoy141 <2345rinoy@gmail.com>
2024-12-11 13:31:06 +0530

    intial

--- lib/food_app/main_page.dart
+++ lib/food_app/main_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'food_provider.dart';
+
 
 
 class MainPage extends StatelessWidget {
@@ -13,36 +13,46 @@ class MainPage extends StatelessWidget {
       create: (context) => BottomNavBarProvider(),
       child: Consumer<BottomNavBarProvider>(
         builder: (context, bottomNavBarProvider, child) {
-          return Scaffold(
-            body: bottomNavBarProvider.currentPage,
-            bottomNavigationBar: BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.search),
-                  label: 'Search',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.shopping_cart),
-                  label: 'Cart',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.history),
-                  label: 'Orders',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person),
-                  label: 'Profile',
-                ),
-              ],
-              currentIndex: bottomNavBarProvider.selectedIndex,
-              selectedItemColor: Colors.blue,
-              unselectedItemColor: Colors.grey,
-              onTap: bottomNavBarProvider.onItemTapped,
-              type: BottomNavigationBarType.fixed,
+          return WillPopScope(
+            onWillPop: () async {
+              if (bottomNavBarProvider.selectedIndex != 0) {
+
+                bottomNavBarProvider.onItemTapped(0);
+                return false;
+              }
+              return true;
+            },
+            child: Scaffold(
+              body: bottomNavBarProvider.currentPage,
+              bottomNavigationBar: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.search),
+                    label: 'Search',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.shopping_cart),
+                    label: 'Cart',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.history),
+                    label: 'Orders',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    label: 'Profile',
+                  ),
+                ],
+                currentIndex: bottomNavBarProvider.selectedIndex,
+                selectedItemColor: Colors.blue,
+                unselectedItemColor: Colors.grey,
+                onTap: bottomNavBarProvider.onItemTapped,
+                type: BottomNavigationBarType.fixed,
+              ),
             ),
           );
         },
